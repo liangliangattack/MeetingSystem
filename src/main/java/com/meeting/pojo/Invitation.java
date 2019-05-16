ea48446ada0af515fc8290abcfc8e15ea39c5ff1
Mercer77 <1053911413@qq.com>
2019-05-16 16:13:02 +0800

    meeting invitation

--- src/main/java/com/meeting/pojo/Invitation.java
+++ src/main/java/com/meeting/pojo/Invitation.java
@@ -1,7 +1,53 @@
 package com.meeting.pojo;
 
 import java.util.Date;
+import java.util.List;
 
 public class Invitation {
+    private Integer id;
+    private Meeting meeting;
+    private List<UserInfo> toAttendUser;
+    private Date initTime;
 
+    @Override
+    public String toString() {
+        return "Invitation{" +
+                "id=" + id +
+                ", meeting=" + meeting +
+                ", toAttendUser=" + toAttendUser +
+                ", initTime=" + initTime +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Meeting getMeeting() {
+        return meeting;
+    }
+
+    public void setMeeting(Meeting meeting) {
+        this.meeting = meeting;
+    }
+
+    public List<UserInfo> getToAttendUser() {
+        return toAttendUser;
+    }
+
+    public void setToAttendUser(List<UserInfo> toAttendUser) {
+        this.toAttendUser = toAttendUser;
+    }
+
+    public Date getInitTime() {
+        return initTime;
+    }
+
+    public void setInitTime(Date initTime) {
+        this.initTime = initTime;
+    }
 }
--- src/main/java/com/meeting/pojo/Meeting.java
+++ src/main/java/com/meeting/pojo/Meeting.java
@@ -3,5 +3,90 @@ package com.meeting.pojo;
 import java.util.Date;
 
 public class Meeting {
+    private Integer id;
+    private String meetingTitle;
+    private String meetingContent;
+    private Date createTime;
+    private UserInfo createByUser;
+    private String time;
+    private String path;
+    private String address;
 
+    @Override
+    public String toString() {
+        return "Meeting{" +
+                "id=" + id +
+                ", meetingTitle='" + meetingTitle + '\'' +
+                ", meetingContent='" + meetingContent + '\'' +
+                ", createTime=" + createTime +
+                ", createByUser=" + createByUser +
+                ", time='" + time + '\'' +
+                ", path='" + path + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMeetingTitle() {
+        return meetingTitle;
+    }
+
+    public void setMeetingTitle(String meetingTitle) {
+        this.meetingTitle = meetingTitle;
+    }
+
+    public String getMeetingContent() {
+        return meetingContent;
+    }
+
+    public void setMeetingContent(String meetingContent) {
+        this.meetingContent = meetingContent;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public UserInfo getCreateByUser() {
+        return createByUser;
+    }
+
+    public void setCreateByUser(UserInfo createByUser) {
+        this.createByUser = createByUser;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
