7ed93043472dbe2d06273c0605227cafe70f4d7e
e <1364744931@qq.com>
2019-05-16 15:40:37 +0800

    框架

--- src/main/java/com/meeting/controller/MeetingController.java
+++ src/main/java/com/meeting/controller/MeetingController.java
@@ -1,15 +1,22 @@
 package com.meeting.controller;
 
+import com.meeting.pojo.Meeting;
+import com.meeting.service.MeetingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
-//@Controller
-//public class MeetingController {
+@Controller
+public class MeetingController {
+
+    @Autowired
+    private MeetingService meetingService;
+
+//    @RequestMapping("/queryAllMeetings/{id}")
+//    public List<Meeting> queryAllMeetings(@PathVariable("id")){
 //
-//    @Autowired
-//    private MeetingService meetingService;
-//
-//    public List<>
-//}
+//    }
+}
